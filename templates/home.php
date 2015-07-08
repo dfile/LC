daedb1f85996b44d8be6e4eccb9c209136ea1246
Daniel File <danielwfile@Langlands.local>
2015-07-08 13:58:24 -0400

    webpage

---
+++ templates/home.php
@@ -0,0 +1,7 @@
+    <div class="container">
+
+      <div class="starter-template">
+        <h1>Daniel W. File</h1>
+		<p class="lead">Visiting Assistant Professor<br>Department of Mathematics and Computer Science <br> Muhlenberg College</p>
+		<img src="dan3.jpg" alt="Dan" height=300>
+      </div>
---
+++ templates/navbar.php
@@ -0,0 +1,11 @@
+        <div class="collapse navbar-collapse">
+          <ul class="nav navbar-nav">
+            <li class="active"><a href="#">Home</a></li>
+            <li><a href="#research">Research</a></li>
+            <li><a href="#teaching">Teaching</a></li>
+			<li><a href='#cv'>Curriculum Vitae</a></li>
+			<li><a href='#travel'>Travel</a></li>
+          </ul>
+        </div><!--/.nav-collapse -->
+      </div>
+    </div>
